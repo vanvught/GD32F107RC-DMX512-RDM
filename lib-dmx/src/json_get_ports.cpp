2eb5a9273d5ca6705b5fa09f9d8cf3da5bd4d2f3
Arjan van Vught <arjan.van.vught@gmail.com>
2024-03-14 15:14:29 +0100

    Sync with local development

--- lib-dmx/src/json_get_ports.cpp
+++ lib-dmx/src/json_get_ports.cpp
@@ -1,8 +1,8 @@
 /**
- * @file dmxstartaddressupdate.cpp
+ * @file json_get_ports.cpp
  *
  */
-/* Copyright (C) 2021 by Arjan van Vught mailto:info@gd32-dmx.org
+/* Copyright (C) 2024 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,13 +24,35 @@
  */
 
 #include <cstdint>
+#include <cstdio>
 
-#include "rdmresponder.h"
+#include "dmx.h"
+#include "dmxconst.h"
+#include "lightset.h"
 
-#include "displayudf.h"
+namespace remoteconfig {
+namespace dmx {
+static uint32_t get_portstatus(const uint32_t nPortIndex, char *pOutBuffer, const uint32_t nOutBufferSize) {
+	const auto direction = Dmx::Get()->GetPortDirection(nPortIndex) == ::dmx::PortDirection::INP ? ::lightset::PortDir::INPUT : ::lightset::PortDir::OUTPUT;
+	auto nLength = static_cast<uint32_t>(snprintf(pOutBuffer, nOutBufferSize,
+			"{\"port\":\"%c\",\"direction\":\"%s\"},",
+			'A' + nPortIndex,
+			lightset::get_direction(direction)));
 
-#include "debug.h"
-
-void RDMResponder::DmxStartAddressUpdate([[maybe_unused]] uint16_t nDmxStartAddress) {
-	DisplayUdf::Get()->Show();
+	return nLength;
 }
+
+uint32_t json_get_ports(char *pOutBuffer, const uint32_t nOutBufferSize) {
+	pOutBuffer[0] = '[';
+	uint32_t nLength = 1;
+
+	for (uint32_t nPortIndex = 0; nPortIndex < ::dmx::config::max::PORTS; nPortIndex++) {
+		nLength += get_portstatus(nPortIndex, &pOutBuffer[nLength], nOutBufferSize - nLength);
+	}
+
+	pOutBuffer[nLength - 1] = ']';
+
+	return nLength;
+}
+}  // namespace dmx
+}  // namespace remoteconfig
