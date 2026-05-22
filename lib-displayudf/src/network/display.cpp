3bc5d383d560d57087a316b88a9c242718f1d3c3
Arjan <Arjan.van.Vught@gmail.com>
2026-05-22 13:54:08 +0200

    Refactor display code and replace udelay
    Major refactor across display modules: replace udelay() calls with timing::DelayUs() and add timing.h; switch console colouring to ansi_colour (replace console::Colours usage and console::Status with printf + ANSI sequences); rename/normalize enums and some identifiers (statusled Mode names, local variable renames); reformat code (brace/style changes) and tidy includes. SPI/I2C display classes updated (SpiLcd, Paint, ST7735S, ST7789, Hd44780) with API/implementation adjustments (DrawLine updated to use signed ints, framebuffer/data write logic clarified). Network display file renamed and namespaces adjusted; display timeout IRQ init behavior conditionally adapted. Bump copyright years to 2026 and apply miscellaneous minor cleanups.

--- lib-displayudf/src/network/display.cpp
+++ lib-displayudf/src/network/display.cpp
@@ -1,8 +1,8 @@
 /**
- * @file network_display.cpp
+ * @file display.cpp
  *
  */
-/* Copyright (C) 2021-2025 by Arjan van Vught mailto:info@gd32-dmx.org
+/* Copyright (C) 2021-2026 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,65 +26,51 @@
 #include "displayudf.h"
 #include "core/protocol/dhcp.h"
 
-namespace net::emac::display
-{
-void Config()
-{
+namespace emac::display {
+void Config() {
     DisplayUdf::Get()->ShowEmacInit();
 }
 
-void Start()
-{
+void Start() {
     DisplayUdf::Get()->ShowEmacStart();
 }
-void Status(bool is_link_up)
-{
+void Status(bool is_link_up) {
     DisplayUdf::Get()->ShowEmacStatus(is_link_up);
 }
-} // namespace net::emac::display
+} // namespace emac::display
 
-namespace network::display
-{
-void Hostname()
-{
+namespace network::display {
+void Hostname() {
     DisplayUdf::Get()->ShowHostName();
 }
 
-void EmacShutdown()
-{
+void EmacShutdown() {
     DisplayUdf::Get()->ShowShutdown();
 }
 
-void DhcpStatus(network::dhcp::State state)
-{
+void DhcpStatus(network::dhcp::State state) {
     DisplayUdf::Get()->ShowDhcpStatus(state);
 }
 } // namespace network::display
 
-namespace network::event
-{
-void LinkUp()
-{
+namespace network::event {
+void LinkUp() {
     DisplayUdf::Get()->ShowIpAddress();
 }
 
-void LinkDown()
-{
+void LinkDown() {
     DisplayUdf::Get()->ShowEmacStatus(false);
 }
 
-void Ipv4AddressChanged()
-{
+void Ipv4AddressChanged() {
     DisplayUdf::Get()->ShowIpAddress();
 }
 
-void Ipv4NetmaskChanged()
-{
+void Ipv4NetmaskChanged() {
     DisplayUdf::Get()->ShowNetmask();
 }
 
-void Ipv4GatewayChanged()
-{
+void Ipv4GatewayChanged() {
     DisplayUdf::Get()->ShowGatewayIp();
 }
 } // namespace network::event
